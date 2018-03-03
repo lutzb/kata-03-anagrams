2e69ceb721a6435d42ef4ccc54a727209ef5d0e1
Bryan Lutz <bryanlutz.212@gmail.com>
2018-03-03 10:44:56 -0500

    Refactor Anagram and add an edge case test

--- src/Anagram.java
+++ src/Anagram.java
@@ -1,32 +1,16 @@
+import java.util.Arrays;
+
 public class Anagram {
 
     public Anagram () { }
 
     public boolean isAnagram(String s1, String s2) {
-        if (s1 == s2) {
-            return true;
-        }
-        if (s1.length() != s2.length()) {
-            return false;
-        } else {
-            for (int i = 0; i < s1.length(); i++) {
-                char s1Char = s1.charAt(i);
+        char[] s1Arry = s1.toCharArray();
+        char[] s2Arry = s2.toCharArray();
 
-                boolean charExistsInS2 = false;
-                for (int j = 0; j < s2.length(); j++) {
-                    char s2Char = s2.charAt(j);
+        Arrays.sort(s1Arry);
+        Arrays.sort(s2Arry);
 
-                    if (s1Char == s2Char) {
-                        charExistsInS2 = true;
-                    }
-                }
-
-                if (!charExistsInS2) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
+        return Arrays.equals(s1Arry, s2Arry);
     }
 }
--- test/AnagramTest.java
+++ test/AnagramTest.java
@@ -1,26 +1,33 @@
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class AnagramTest {
 
+    private Anagram anagram;
+
+    @Before
+    public void setup() {
+         this.anagram = new Anagram();
+    }
+
     @Test
     public void whenAnagramIsPassedTheSameStringItReturnsTrue() {
-        Anagram anagram = new Anagram();
-        boolean result = anagram.isAnagram("s", "s");
-        assertEquals(true, result);
+        assertEquals(true, anagram.isAnagram("s", "s"));
     }
 
     @Test
     public void whenAnagramIsPassedTwoStringsOfDifferentSizesItReturnsFalse() {
-        Anagram anagram = new Anagram();
-        boolean result = anagram.isAnagram("ab", "abc");
-        assertEquals(false, result);
+        assertEquals(false, anagram.isAnagram("ab", "abc"));
     }
 
     @Test
     public void whenAnagramIsPassedTwoStringsThatAreAnagramsItReturnsTrue() {
-        Anagram anagram = new Anagram();
-        boolean result = anagram.isAnagram("abcde", "cbeda");
-        assertEquals(true, result);
+        assertEquals(true, anagram.isAnagram("abcde", "cbeda"));
+    }
+
+    @Test
+    public void whenAnagramIsPassedTwoStringsThatAreAnagramsWithDuplicateCharactersItReturnsTrue() {
+        assertEquals(true, anagram.isAnagram("abcdeefg", "ebacdefg"));
     }
 }
